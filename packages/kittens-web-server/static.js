ee4eb48c6991ceaf8df73e1e2365a4416c422d76
Lalo Martins <lalo.martins@gmail.com>
2019-06-29 11:02:50 +0300

    make the fallback serving slightly more intelligent

--- packages/kittens-web-server/static.js
+++ packages/kittens-web-server/static.js
@@ -1,12 +1,12 @@
+import {existsSync} from 'fs';
 import {dirname, resolve} from 'path';
 import {fileURLToPath} from 'url';
 import express from 'express';
 import etbms from 'express-transform-bare-module-specifiers';
 const transformMiddleware = typeof etbms === 'function' ? etbms : etbms.default;
 
-export async function faviconServer(req, res) {
+export function faviconServer(req, res) {
   res.redirect(301, '/assets/favicon.ico');
-  res.send('Please see /assets/favicon.ico');
 }
 
 const COMPONENT_ROOT = resolve(
@@ -25,14 +25,30 @@ const STATIC_ROOT = resolve(process.env.STATIC_ROOT || '../kittens-web/build');
 
 export const staticServer = express.static(STATIC_ROOT);
 
+export const fallbackStaticServer = express.static(STATIC_ROOT, {
+  redirect: false,
+});
+// fallback everything to / and treat it as a SPA (let the front-end deal with 404s)
 export function fallbackServer(req, res, next) {
   const parts = req.path.split('/');
   if (FALLBACK_BLACKLIST.includes(parts[1])) {
     return next();
   } else {
     console.log(`Falling back for ${req.path}`);
-    req.originalUrl = req.url = '/';
-    return staticServer(req, res, next);
+    parts.pop();
+    while (parts.length) {
+      const index = resolve(
+        STATIC_ROOT + parts.concat(['index.html']).join('/')
+      );
+      // console.log(`trying ${index}`);
+      if (existsSync(index)) {
+        req.originalUrl = req.url = parts.join('/') + '/';
+        return fallbackStaticServer(req, res, next);
+      }
+      parts.pop();
+    }
+    // shouldn't happen — if you're here it means you have no /index.html
+    return next();
   }
 }
 
